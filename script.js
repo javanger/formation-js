0703269087d8529e3423a67ebfd020848c083ed3
Valentin <lefebvre.valentin76@gmail.com>
2018-03-21 17:31:05 +0100

    Commit delete

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ const req = new XMLHttpRequest();
   req.open('GET', 'http://app-6443389f-f19e-4b11-80f6-6975352681e5.cleverapps.io/users', true);
   req.send(null);
 
-  document.querySelector('button').addEventListener('click', function(evt){
+  document.querySelector('#add').addEventListener('click', function(evt){
     evt.preventDefault();
 
     const requete = new XMLHttpRequest();
@@ -38,4 +38,50 @@ const req = new XMLHttpRequest();
     requete.open('POST', 'http://app-6443389f-f19e-4b11-80f6-6975352681e5.cleverapps.io/users', true);
     requete.setRequestHeader("Content-type", 'application/json');
     requete.send(variable);
+  })
+
+  function deleteUndefined(id) {
+
+      var xhr = new XMLHttpRequest();
+      var url = "http://app-6443389f-f19e-4b11-80f6-6975352681e5.cleverapps.io/users/" + id;
+      xhr.open("DELETE", url, true);
+      xhr.setRequestHeader("Content-type", "application/json");
+      xhr.onreadystatechange = function () {
+          if (xhr.readyState === 4 && xhr.status === 200) {
+              var json = JSON.parse(xhr.responseText);
+          }
+      };
+      xhr.send();
+
+  }
+  document.querySelector('#suppr').addEventListener('click', function(evt){
+          evt.preventDefault();
+          const req3 = new XMLHttpRequest();
+          req3.onreadystatechange = function (event) {
+                // XMLHttpRequest.DONE === 4
+               if (this.readyState === XMLHttpRequest.DONE) {
+                   if (this.status === 200) {
+                      console.log("Réponse reçue: %s", this.responseText);
+                      var reponse = this.responseText;
+
+                      var tabDelete = JSON.parse(reponse);
+
+                       var compteur = 0
+
+                       tabDelete.forEach(personne => {
+                           if(personne.name === "undefined" ||personne.name === undefined || personne.name === "" || personne.password === "") {
+                               deleteUndefined(personne.id);
+                               compteur += 1;
+                           }
+
+                       });
+
+                       alert("Suppression de " + compteur + " personnes");
+                   } else {
+                       console.log("Status de la réponse: %d (%s)", this.status, this.statusText);
+                   }
+               }
+           };
+          req3.open('GET', 'http://loisirs-web-backend.cleverapps.io/users', true);
+          req3.send(null);
   })
